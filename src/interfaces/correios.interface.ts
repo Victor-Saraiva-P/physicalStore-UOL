289d27a28e22bcfb165cd5fc42c6a15bbe7f4b3a
Victor Saraiva <victor_alex2005@hotmail.com>
2025-01-08 16:24:39 -0300

    ✨ Adiciona API dos correios para cálculo de preço e prazo de entrega e define interfaces correspondentes

---
+++ src/interfaces/correios.interface.ts
@@ -0,0 +1,19 @@
+export interface PrecoPrazoItem {
+  status: number;
+  mensagemPrecoAgencia: string;
+  prazo: string;
+  url: string;
+  mensagemPrecoPPN: string;
+  codProdutoAgencia: string;
+  precoPPN: string;
+  codProdutoPPN: string;
+  mensagemPrazo: string;
+  msg: string;
+  precoAgencia: string;
+  urlTitulo: string;
+}
+
+export interface PrecoPrazoResponse {
+  sedex: PrecoPrazoItem;
+  pac: PrecoPrazoItem;
+}
