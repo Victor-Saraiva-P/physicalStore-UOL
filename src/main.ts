3f75846f94626373776a7482ff161e27d507a9c1
Victor Saraiva <victor_alex2005@hotmail.com>
2025-01-10 11:24:22 -0300

    📚 Adiciona suporte ao Swagger para documentação da API e atualiza dependências no package.json e package-lock.json

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { validateEnvVariables } from '@validators/env-variables.validator';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   // Validando as variáveis de ambiente
   validateEnvVariables();
 
   const app = await NestFactory.create(AppModule);
+
+  // Configuração do Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Physical Store API')
+    .setDescription('API para gerenciar lojas físicas e calcular distâncias')
+    .setVersion('1.0')
+    .addTag('stores') // Adicione tags relevantes para organizar os endpoints
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Acessível em /api
 
   // Habilitando o class-validator
   app.useGlobalPipes(new ValidationPipe());
